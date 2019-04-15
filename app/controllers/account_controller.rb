98c53f47f3a7ee4c22afbae78c7211caef0c0f68
Jules LAURENT <jules.laurent@ynov.com>
2019-04-15 09:32:30 +0200

    add change password form

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,2 +1,32 @@
 class AccountController < ApplicationController
+  def profile
+    @user = current_user
+
+  end
+  def contacts
+
+  end
+  def messages
+
+  end
+
+  before_action :authenticate_user!
+
+  def update_password
+    @user = current_user
+    if @user.update(user_params)
+      # Sign in the user by passing validation in case their password changed
+      bypass_sign_in(@user)
+      redirect_to root_path
+    else
+      render "profile"
+    end
+  end
+
+  private
+
+  def user_params
+    # NOTE: Using `strong_parameters` gem
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'home#index'
+  get 'accounts/profile', to: 'account#profile'
+  get 'accounts/messages', to: 'account#messages'
+  get 'accounts/contacts', to: 'account#contacts'
+  patch 'accounts/update_password', to: 'account#update_password'
 end
