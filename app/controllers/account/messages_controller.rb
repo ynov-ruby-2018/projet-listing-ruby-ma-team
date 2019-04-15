18dc5e44907b9c9f7a6b5a6a2fa4c29ccc35bdc4
h3r0xfr <adrien.cabaud@numericable.fr>
2019-04-15 16:29:01 +0200

    add account css

--- app/controllers/account/messages_controller.rb
+++ app/controllers/account/messages_controller.rb
@@ -1,4 +1,4 @@
-class Account::MessagesController < AccountController
+class Account::MessagesController < Account::AccountController
   def index
     @user = current_user
     @messages = Message.where(user: @user)
