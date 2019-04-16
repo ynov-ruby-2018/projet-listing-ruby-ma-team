7506c53802fc2a325260e1ec142a70ba0b5862f2
h3r0xfr <adrien.cabaud@numericable.fr>
2019-04-16 16:15:03 +0200

    setup deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,10 +25,10 @@ set :branch, "develop"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
