baf9b8eff9baab76b4f849456868605c9a4f3958
h3r0xfr <adrien.cabaud@numericable.fr>
2019-04-16 15:49:32 +0200

    disable api

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,21 +1,21 @@
 # app/controllers/api/v1/auth_controller.rb
 
-class Api::V1::AuthController < Api::V1::ApiController
-
-  def create
-    user = User.find_by(email: params[:email])
-    token = nil
-
-    if user
-      if user.valid_password?(params[:password])
-        token = user.generate_token
-      end
-    end
-
-    if token
-      render json: {success: true, token: token}
-    else
-      render json: {success: false, errors: [{ user: "Mot de passe invalide" }]}, status: 401
-    end
-  end
-end
+# class Api::V1::AuthController < Api::V1::ApiController
+#
+#   def create
+#     user = User.find_by(email: params[:email])
+#     token = nil
+#
+#     if user
+#       if user.valid_password?(params[:password])
+#         token = user.generate_token
+#       end
+#     end
+#
+#     if token
+#       render json: {success: true, token: token}
+#     else
+#       render json: {success: false, errors: [{ user: "Mot de passe invalide" }]}, status: 401
+#     end
+#   end
+# end
