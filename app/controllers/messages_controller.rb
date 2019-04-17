2e717c26e63ab1a93bd669bf460e1b0c64aee3f6
Jules LAURENT <jules.laurent@ynov.com>
2019-04-17 15:29:57 +0200

    prevent empty  messages from sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,17 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def create
-    @message = Message.new(message_params)
-    @message.user = current_user
-    @message.announcement
-    @message.save!
-    flash[:notice] = t('message.success')
-    redirect_to announcements_path
+    if(message_params['content'] == nil || message_params['content'] == '')
+      flash[:alert] = t('message.failure')
 
+    else
+      @message = Message.new(message_params)
+      @message.user = current_user
+      @message.announcement
+      @message.save!
+      flash[:notice] = t('message.success')
+    end
+    redirect_to announcements_path
   end
 
   def message_params
