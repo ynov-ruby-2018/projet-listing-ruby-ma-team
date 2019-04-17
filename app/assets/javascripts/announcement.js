ff589fdd61a105b63e9e369c6dbb80f894acc054
Jules LAURENT <jules.laurent@ynov.com>
2019-04-17 14:39:51 +0200

    add hide/show modal handlers

---
+++ app/assets/javascripts/announcement.js
@@ -0,0 +1,22 @@
+console.log('ok');
+document.addEventListener("DOMContentLoaded", function(event) {
+    let elements = document.querySelectorAll('.contact-modal-toggle');
+    elements.forEach(element => {
+        element.addEventListener('click',openModalEventListener);
+    });
+
+    function openModalEventListener(event) {
+        let modal = document.querySelector(`#${event.target.attributes['data-target'].value}`);
+        modal.classList.add('is-active');
+    }
+    function closeModalEventListener(event) {
+        event.target.parentElement.classList.remove('is-active');
+
+    }
+
+    let close = document.querySelectorAll('.modal-close');
+    close.forEach(element => {
+        element.addEventListener('click',closeModalEventListener);
+    });
+
+});
